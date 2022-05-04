5b17ac6e63707fc851646805f5b2ac0407e9f616
ajhar10 <ajharrimon90@gmail.com>
2022-05-05 00:55:48 +0600

    All Require Installation done

---
+++ src/firebase.init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB8q4QoflgEw1RBtRSZp1POsonJK1GsDog",
+    authDomain: "tirez-bfac7.firebaseapp.com",
+    projectId: "tirez-bfac7",
+    storageBucket: "tirez-bfac7.appspot.com",
+    messagingSenderId: "242898591846",
+    appId: "1:242898591846:web:0a30e314e90fab628e0da7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
