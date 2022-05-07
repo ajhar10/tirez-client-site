fdad5608125791c83fed9d5581eac1fa2876cf7b
ajhar10 <ajharrimon90@gmail.com>
2022-05-07 17:45:28 +0600

    inventory page

--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -5,12 +5,14 @@ import './Products.css'
 
 const Products = () => {
     const [products, setProducts] = useProduct();
+    const homeProduct = products.slice(0, 6);
     return (
-        <div className='products'>
+        <div className='products py-4'>
+            <h3 className='fw-bold'>OUR <span className='fs-2' style={{ color: "orange" }}>PRODUCTS </span></h3>
             <div className="container-fluid bg-trasparent my-4 p-3" style={{ position: "relative" }}>
                 <div className="row row-cols-1 row-cols-xs-2 row-cols-sm-2 row-cols-lg-3 g-3">
                     {
-                        products.map(product => <Product key={product._id} product={product}></Product>)
+                        homeProduct.map(product => <Product key={product._id} product={product}></Product>)
                     }
 
                 </div>
--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Form } from 'react-bootstrap';
+import { Link, useParams } from 'react-router-dom';
 
 const Inventory = () => {
     const { id } = useParams();
-    const [product, setProduct] = useState({})
+    const [product, setProduct] = useState({});
+    const url = `http://localhost:5000/products/${id}`;
     useEffect(() => {
-        const url = `http://localhost:5000/products/${id}`;
         fetch(url)
             .then(res => res.json())
             .then(data => setProduct(data))
@@ -13,10 +14,39 @@ const Inventory = () => {
 
 
     return (
-        <div>
-            {
-                product.name
-            }
+        <div class="container">
+            <div class="row" style={{ margin: '100px 0' }}>
+                <div className="col-sm-12 col-md-12 col-lg-8">
+                    <div className="card h-100 shadow-sm w-75">
+                        <img src={product.image} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h6>Name - {product.name}</h6>
+                            <div className="clearfix mb-3">
+                                <span className="float-start badge rounded-pill bg-info">{product.quantity} available</span>
+                                <span className="float-end price-hp">{product.price}$</span>
+                            </div>
+                            <h4 className="card-title">{product.description}</h4>
+                            <h6>Supplier - {product.supplier}</h6>
+                            <div className="text-center my-4">
+                                <button className='btn btn-danger'>Delivered</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-12 col-lg-4 my-5">
+                    <h4>Restock</h4>
+                    <form className='p-3' style={{ backgroundColor: "rgba(0,0,255,0.1)" }}>
+                        <input className='rounded border-0 ' type="number" name="quantity" placeholder='quantity' required /><br />
+                        <input className='btn btn-danger my-3' type="submit" value="Restoke" />
+                    </form>
+                </div>
+            </div>
+
+            <div class="row" style={{ margin: '100px 0' }}>
+                <div className="col-sm-12 col-md-12 col-lg-12 d-flex justify-content-center align-items-center">
+                    <Link to="/inventory" className="btn btn-info">Manage Inventory</Link>
+                </div>
+            </div>
         </div>
     );
 };
