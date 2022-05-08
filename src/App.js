58a2a8fe73b57c27ca39cf705ada29407bfe06c6
ajhar10 <ajharrimon90@gmail.com>
2022-05-08 17:59:17 +0600

    readme set

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
 import AddItem from './Pages/AddItem/AddItem';
 import Blogs from './Pages/Blogs/Blogs';
 import Home from './Pages/Home/Home/Home';
@@ -23,6 +24,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
 
         {/* <Route path='/products' element={<Product></Product>}></Route> */}
         <Route path='/inventory/:id' element={
---
+++ src/Pages/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="container">
+            <h1 className="text-center">About Me</h1>
+            <p>An exciting and creative position in a service organization where there is opportunity to work with people in an environment
+                of excellence & passions and thus build a better career and fulfill mission, vision, objective & goal of the organization.</p>
+        </div>
+    );
+};
+
+export default About;
